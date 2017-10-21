126013df6a9aa737d7345d5f6284c5938c35b847
tossu <malanojonen@gmail.com>
2017-10-22 02:19:17 +0300

    cleaned

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Kauppalehti OMXH
 
 Scraper for kauppalehti OMXH data.
-
-## Issues:
-- '-' symbols in change fields
-- volume given in wierd format
--- src/kauppalehti_omxh/core.clj
+++ src/kauppalehti_omxh/core.clj
@@ -1,38 +1,24 @@
 (ns kauppalehti-omxh.core
   (:require [net.cgrand.enlive-html :as html]))
 
-(defn parse-number
-  [s]
+(defn- parse-number [s]
   (if (re-find #"^-?\d+\.?\d*$" s)
     (read-string s)))
 
-;; (defn get-dom
-;;   []
-;;   (html/html-snippet
-
-(defn parse-value
-  [stock, selector]
+(defn- parse-value [stock, selector]
   (html/text (first (html/select stock [selector]))))
 
-(defn parse-stock
-    [stock]
+(defn- parse-stock [stock]
     { :name (parse-value stock :span.stock-item-name)
-      :volume (parse-value stock :span.stock-item-volume)
-      :change (parse-value stock :span.stock-item-change)
       :value (parse-number (parse-value stock :span.stock-item-value))})
 
-(defn parse-stocks
-    [dom]
+(defn- parse-stocks [dom]
     (map (fn [stock-dom] (parse-stock stock-dom))
          (html/select dom [:div.stock-item])))
 
-(defn stocks
-  [html]
+(defn stocks [html]
+  "http://app.kauppalehti.fi/market/stockexchange"
   (parse-stocks (html/html-snippet html)))
 
-(defn -main
-  [& args]
-;;  (let [ dom (:body @(http/get "http://app.kauppalehti.fi/market/stockexchange"
-;;                               {:insecure? true}))]
-  (let [dom (slurp "kauppalehti.htm")]
-    (println (stocks dom))))
+(defn -main [& args]
+  (println (stocks (slurp "kauppalehti.htm"))))
